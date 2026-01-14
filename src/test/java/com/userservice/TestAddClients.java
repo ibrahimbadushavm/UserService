ecec73c1f0ef9d60eabb1f94d86b741540aa3e0c
ibrahimbadushavm <ibrahimbadu779@gmaail.com>
2026-01-14 19:36:19 +0530

    env variables

--- src/test/java/com/userservice/TestAddClients.java
+++ src/test/java/com/userservice/TestAddClients.java
@@ -17,27 +17,27 @@ import java.util.UUID;
 @SpringBootTest
 public class TestAddClients {
 
-    @Autowired
-    private JpaRegisteredClientRepository jpaRegisteredClientRepository;
+//    @Autowired
+//    private JpaRegisteredClientRepository jpaRegisteredClientRepository;
+//
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Test
-    public void testAddClients() {
-        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
-               .clientId("productservice-client")
-               .clientSecret(passwordEncoder.encode("productservicepassword"))
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-                .redirectUri("https://oauth.pstmn.io/v1/callback")
-                .postLogoutRedirectUri("http://localhost:9000")
-                .scope(OidcScopes.OPENID)
-                .scope(OidcScopes.PROFILE)
-                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
-                .build();
-
-        jpaRegisteredClientRepository.save(oidcClient);
-    }
+//    @Test
+//    public void testAddClients() {
+//        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+//               .clientId("productservice-client")
+//               .clientSecret(passwordEncoder.encode("productservicepassword"))
+//                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+//                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+//                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+//                .redirectUri("https://oauth.pstmn.io/v1/callback")
+//                .postLogoutRedirectUri("http://localhost:9000")
+//                .scope(OidcScopes.OPENID)
+//                .scope(OidcScopes.PROFILE)
+//                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+//                .build();
+//
+//        jpaRegisteredClientRepository.save(oidcClient);
+//    }
 }
--- src/test/java/com/userservice/TestAddUserRole.java
+++ src/test/java/com/userservice/TestAddUserRole.java
@@ -14,26 +14,26 @@ import java.util.List;
 @SpringBootTest
 public class TestAddUserRole {
 
-    @Autowired
-    private RoleRepository roleRepository;
+//    @Autowired
+//    private RoleRepository roleRepository;
+//
+//    @Autowired
+//    private UserRepository userRepository;
 
-    @Autowired
-    private UserRepository userRepository;
+//    @Test
+//    void testAddRole() {
+//        // Implement test logic to add user role
+//        Role role = new Role();
+//        role.setRoleName("ADMIN");
+//        roleRepository.save(role);
+//    }
 
-    @Test
-    void testAddRole() {
+//    @Test
+//    void testAddUserRoleUser() {
         // Implement test logic to add user role
-        Role role = new Role();
-        role.setRoleName("ADMIN");
-        roleRepository.save(role);
-    }
-
-    @Test
-    void testAddUserRoleUser() {
-        // Implement test logic to add user role
-        User user= userRepository.findById(1L).orElse(null);
-        Role role = roleRepository.findById(1L).orElse(null);
-        user.setRoles(new HashSet<>(List.of(role)));
-        userRepository.save(user);
-    }
+//        User user= userRepository.findById(1L).orElse(null);
+//        Role role = roleRepository.findById(1L).orElse(null);
+//        user.setRoles(new HashSet<>(List.of(role)));
+//        userRepository.save(user);
+//    }
 }
